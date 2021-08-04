0d214850e481046aff5c29bf9098b697399beac4
Kaz DeBear <kazdb@me.com>
2021-08-04 10:34:15 -0700

    page

--- src/index.js
+++ src/index.js
@@ -258,10 +258,10 @@ let benSvg = d3.select(".benefits-graph")
 d3.csv("src/assets/data/benefits.csv", function(benData) {
 
   let root = d3.stratify()
-    .id(function(d) { return d.name;})
+    .id(function(d) { return d.name; })
     .parentId(function(d) { return d.parent; })
     (benData);
-  root.sum(function(d) { return +d.value })
+  root.sum(function(d) { return +d.value; })
 
   d3.treemap()
     .size([benWidth, benHeight])
