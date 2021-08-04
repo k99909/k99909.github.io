0e7f3aa7ff2c955754f8b6fd4a820a86faa28407
Kaz DeBear <kazdb@me.com>
2021-08-04 10:39:50 -0700

    page

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ const zenColors = [
 d3.select('#quote-btn')
         .on('click', function () {
             let randAuth = authors[Math.floor(Math.random() * authors.length)];
-            let randQuote = quotes[randAuth][Math.floor(Math.random() * quotes[randAuth].length)]
+            let randQuote = quotes[randAuth][Math.floor(Math.random() * quotes[randAuth].length)];
             let randColor = zenColors[Math.floor(Math.random() * zenColors.length)]
             d3.select('#quote-text').text(randQuote)
             d3.select('.mb-author').text(randAuth)
